9e36217acb9460224861903d3b93d57947900192
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-06-16 22:33:30 +0800

    Updated Program.cs
    added DbContext

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using TailMates.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TailMatesContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
