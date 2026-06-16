9ec84ef040c54a84f3d5b62233527decd5199a8a
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-06-16 22:28:32 +0800

    Created DbContext

---
+++ Data/TailMatesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TailMates.Data
+{
+    public class TailMatesContext : DbContext
+    {
+        public TailMatesContext(DbContextOptions<TailMatesContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
