6d11995b8d9ea118771775b5040d6d396b1004bb
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-17 17:30:19 +0800

    Created User Entity Model

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TailMates.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? MiddleName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
