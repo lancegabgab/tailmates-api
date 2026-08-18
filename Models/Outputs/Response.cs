730e72ac8903de9e5edcf59549026ad780d096cc
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-18 18:44:31 +0800

    Update Response.cs

--- Models/Outputs/Response.cs
+++ Models/Outputs/Response.cs
@@ -1,8 +1,9 @@
 ﻿namespace TailMates.Models.Outputs
 {
-    public class Response
+    public class Response<T>
     {
         public bool Success { get; set; }
         public string Message { get; set; } = String.Empty;
+        public T? Data { get; set; }
     }
 }
